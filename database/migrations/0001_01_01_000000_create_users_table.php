6c754a5ed279201c5e0b85e3e4eba2c74d477f72
Selimaks <yavuzselimaksoy01@gmail.com>
2025-02-15 23:32:08 +0300

    deploy aşaması

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id()->primary();
+            $table->id();
             $table->string('name');
             $table->string('phone_number', 20)->unique()->nullable();
             $table->enum('gender', ['male', 'female'])->nullable();
